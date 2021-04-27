24667890b00c81e5b25c797343f1a75f62046f97
thiagoaugusto <thiago.android88@gmail.com>
2021-04-27 14:10:50 -0300

    Data Seeder Script

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -73,6 +73,6 @@ const orderSchema = new mongoose.Schema(
   }
 );
 
-const Order = mongoose.model('Order', userSchema);
+const Order = mongoose.model('Order', orderSchema);
 
 export default Order;
