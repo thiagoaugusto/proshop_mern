139aeae8c31e47d00ad892a4a33d4d40897bb461
thiagoaugusto <thiago.android88@gmail.com>
2021-04-27 22:44:05 -0300

    Custom Error Handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -17,18 +17,19 @@ router.get(
 );
 
 // @desc    Fetch single product
-// route    GET /api/products/:id
-// access   Public
+// @route    GET /api/products/:id
+// @access   Public
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not Found');
     }
-
-    res.status(200).json({ product });
   })
 );
 
