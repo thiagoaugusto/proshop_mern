d6120cb5774a1ddea13cc78f33325ae105f0420d
thiagoaugusto <thiago.android88@gmail.com>
2021-04-30 23:48:08 -0300

    Cart Reducer & Add To Cart Action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,33 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM: {
+      const newItem = action.payload;
+
+      const existItem = state.cartItems.find(
+        (cartItem) => cartItem.product === newItem.product
+      );
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((cartItem) =>
+            cartItem.product === newItem.product ? cartItem : newItem
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, action.payload],
+        };
+      }
+    }
+    case CART_REMOVE_ITEM: {
+      return {};
+    }
+
+    default:
+      return state;
+  }
+};
