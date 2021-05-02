c5d91ff23ff6b14c5358dbfb83fdd52363796030
thiagoaugusto <thiago.android88@gmail.com>
2021-05-02 11:40:00 -0300

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from '../backend/config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
@@ -11,6 +12,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 
 app.use(notFound);
