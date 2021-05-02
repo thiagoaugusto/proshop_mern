b1954b539177c7fafa49423bbd33bb9c037e7614
thiagoaugusto <thiago.android88@gmail.com>
2021-05-02 10:31:33 -0300

    Clean Up By Using Controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
+
+// @desc    Fetch all products
+// route    GET /api/products
+// access   Public
+const getProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find({});
+
+  res.status(200).json(products);
+});
+
+// @desc    Fetch single product
+// @route    GET /api/products/:id
+// @access   Public
+const getProductById = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not Found');
+  }
+});
+
+export { getProducts, getProductById };
