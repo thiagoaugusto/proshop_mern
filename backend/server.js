a54cce418df91efc4862d8e39ceb2e9487da7d81
thiagoaugusto <thiago.android88@gmail.com>
2021-05-09 20:34:13 -0300

    Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from '../backend/config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
