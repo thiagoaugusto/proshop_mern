879a43f1545e2ad50e059c69f0e42854f92ec9f0
thiagoaugusto <thiago.android88@gmail.com>
2021-05-29 11:15:33 -0300

    Morgan & Create Review Endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
   deleteProductById,
   createProduct,
   updateProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -16,5 +17,7 @@ router
   .get(getProductById)
   .delete(protect, admin, deleteProductById)
   .put(protect, admin, updateProduct);
+
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
